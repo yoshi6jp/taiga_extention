a12770d7c928cc154976dac6507a30293be83843
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2019-01-14 21:08:12 +0900

    推定量のランクを追加

--- src/UserTasks.tsx
+++ src/UserTasks.tsx
@@ -5,7 +5,9 @@ import { RootContext, baseUrl } from './Provider';
 import { Table, Button } from 'reactstrap';
 import _ from 'lodash';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSyncAlt, faMedal } from '@fortawesome/free-solid-svg-icons';
+import styles from './UserTasks.module.css';
+import moment from 'moment';
 
 const getTasksByUser = (items: ITask[]) => _.groupBy(items, 'assigned_to');
 export const parseCustomVal = (val: string) =>
@@ -31,7 +33,60 @@ export const getCustomVal = (
     return 0;
   }
 };
+const getGrade = (e: any, r: any): [string | null, number] => {
+  if (_.isNumber(e) && _.isNumber(r) && e > 0) {
+    const diff = Math.abs(e - r) / e;
+    if (diff <= 0.05) {
+      return ['gold', 3];
+    }
+    if (diff <= 0.1) {
+      return ['silver', 2];
+    }
+    if (diff < 0.2) {
+      return ['bronze', 1];
+    }
+  }
+  return [null, 0];
+};
+const Medal = ({ e, r }: { e: any; r: any }) => {
+  const [grade, num] = getGrade(e, r);
+  if (grade) {
+    return (
+      <>
+        {_.times(num).map(i => (
+          <FontAwesomeIcon key={i} className={styles[grade]} icon={faMedal} />
+        ))}
+      </>
+    );
+  } else {
+    return null;
+  }
+};
 
+const UserRow = ({
+  item,
+  sums,
+  isPast
+}: {
+  item: IUser;
+  sums: { [key: string]: { e: number; r: number } };
+  isPast: boolean;
+}) => {
+  const e = _.get(sums, `${item.id}.e`);
+  const r = _.get(sums, `${item.id}.r`);
+  return (
+    <tr key={item.id}>
+      <td>{item.username}</td>
+      <td className="text-right">{e}</td>
+      <td className="text-right">{r}</td>
+      {isPast ? (
+        <td>
+          <Medal e={e} r={r} />
+        </td>
+      ) : null}
+    </tr>
+  );
+};
 const getTaskSumByUser = (
   items: ITask[],
   custom_value_map: ICustomValueMap,
@@ -65,7 +120,8 @@ export const UserTasks = () => {
       custom_value_map,
       custom_attrs,
       custom_eid,
-      custom_rid
+      custom_rid,
+      biz_days
     },
     updateData
   } = useContext(RootContext);
@@ -91,10 +147,11 @@ export const UserTasks = () => {
   );
   const customAttrE = getCustomAttr(custom_attrs, Number(custom_eid));
   const customAttrR = getCustomAttr(custom_attrs, Number(custom_rid));
-  if (!customAttrE || !customAttrR) {
+  if (!customAttrE || !customAttrR || biz_days.length <= 1) {
     return null;
   }
   const unassignedSum = _.get(taskSumByUser, 'null.e', 0);
+  const isPast = moment().diff(moment(_.last(biz_days))) > 0;
   return (
     <>
       <div className="text-right">
@@ -108,24 +165,23 @@ export const UserTasks = () => {
             <th>Name</th>
             <th>{customAttrE.name}</th>
             <th>{customAttrR.name}</th>
+            {isPast ? <th>Grade</th> : null}
           </tr>
         </thead>
         <tbody>
           {items.map(item => (
-            <tr key={item.id}>
-              <td>{item.username}</td>
-              <td className="text-right">
-                {_.get(taskSumByUser, `${item.id}.e`)}
-              </td>
-              <td className="text-right">
-                {_.get(taskSumByUser, `${item.id}.r`)}
-              </td>
-            </tr>
+            <UserRow
+              key={item.id}
+              isPast={isPast}
+              item={item}
+              sums={taskSumByUser}
+            />
           ))}
           <tr key="null">
             <td>unassigned</td>
             <td className="text-right text-danger">{unassignedSum}</td>
             <td />
+            {isPast ? <td /> : null}
           </tr>
         </tbody>
       </Table>
