c8ed9795a655c0e7c5f35f518652a50be117f874
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2019-05-13 10:31:46 +0900

    remove react-toastr

--- src/components/Toastr.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useRef } from "react";
-import { ToastContainer, ToastMessageAnimated } from "react-toastr";
-const toastMessageFactory = React.createFactory(ToastMessageAnimated);
-export const Toastr: React.FC = () => {
-  const ref = useRef<ToastContainer>(null);
-  return <ToastContainer ref={ref} toastMessageFactory={toastMessageFactory} />;
-};
