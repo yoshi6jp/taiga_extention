22292f50cc6395a17a6668ede5618e6f5a4b60a3
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2019-05-15 16:18:38 +0900

    Updates

---
+++ precache-manifest.55688463ec2ffde5c664bbf42cfc4bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5239bdf147953158634a58ff7b7da0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72078c150cb32e7262c4",
+    "url": "./static/css/2.e12951e0.chunk.css"
+  },
+  {
+    "revision": "5710142e61801af47a0d",
+    "url": "./static/css/main.9e5fd2ae.chunk.css"
+  },
+  {
+    "revision": "72078c150cb32e7262c4",
+    "url": "./static/js/2.5536311d.chunk.js"
+  },
+  {
+    "revision": "5710142e61801af47a0d",
+    "url": "./static/js/main.a779f8df.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f5a780750cf0bdef245e29016751e8af.js"
+  "./precache-manifest.55688463ec2ffde5c664bbf42cfc4bd4.js"
 );
 
 self.addEventListener('message', (event) => {
