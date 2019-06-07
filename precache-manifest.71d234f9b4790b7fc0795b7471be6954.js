af092189da6c50da19b108e67ed4854497effafc
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2019-06-07 19:14:07 +0900

    Updates

---
+++ precache-manifest.71d234f9b4790b7fc0795b7471be6954.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09a1a4b6987529d071951e37b89f41e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fadfdea646ce60f5f083",
+    "url": "./static/css/2.c17c5eb3.chunk.css"
+  },
+  {
+    "revision": "a0153a12fb2269a94850",
+    "url": "./static/css/main.a46c330a.chunk.css"
+  },
+  {
+    "revision": "fadfdea646ce60f5f083",
+    "url": "./static/js/2.f6ead4a2.chunk.js"
+  },
+  {
+    "revision": "a0153a12fb2269a94850",
+    "url": "./static/js/main.3174d3dc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "998f40c576edb3279bc7acfbc02e1317",
+    "url": "./static/media/tomato.998f40c5.svg"
+  },
+  {
+    "revision": "c392a271814da7d3f533fe0f09294d9f",
+    "url": "./static/media/tomato.c392a271.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a1fcd15dbb6c14060e4c7beaabe83733.js"
+  "./precache-manifest.71d234f9b4790b7fc0795b7471be6954.js"
 );
 
 self.addEventListener('message', (event) => {
