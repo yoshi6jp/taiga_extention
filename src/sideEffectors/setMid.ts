3ae4b9c95c9c1fed990a74456dcf050cab5e7ebc
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2019-06-12 19:57:21 +0900

    スプリントが変更された場合に、選択されたタスクをリセットする

--- src/sideEffectors/setMid.ts
+++ src/sideEffectors/setMid.ts
@@ -4,10 +4,14 @@ import _ from "lodash";
 export const setMid: ISideEffector = (action, dispatch, state) => {
   if (action.type === ActionTypes.SET_MID) {
     const { mid } = action.payload;
+    const beforeMid = state().mid;
     setToStorage(StorageKey.MID, mid);
     const biz_days = _.compact(
       getFromStorageWithSubkey(StorageKey.BIZ_DAYS, mid).split(",")
     ).sort();
     dispatch({ type: ActionTypes.SET_BIZ_DAYS, payload: { biz_days } });
+    if (beforeMid && mid !== beforeMid) {
+      dispatch({ type: ActionTypes.RESET_TASK_ID });
+    }
   }
 };
