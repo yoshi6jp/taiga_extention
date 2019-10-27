c4b4d19f60114bdc065c207fd58d828a67c5c6c8
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2019-10-27 11:19:03 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cfa1221a1f221e8c8e1ceaec62d6e4d9.js"
+  "./precache-manifest.c109fb4f21941eb32676d7c9f9a9298e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
