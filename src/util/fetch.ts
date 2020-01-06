d0dae189c83d5d1efca641a0497559a5ed82911a
鎌田義春 <kamatayoshiharu@kamatayoshiharunoMacBook-Pro.local>
2020-01-06 23:35:34 +0900

    headerの追加

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -1,6 +1,7 @@
 import { fromFetch } from "rxjs/fetch";
 import { mergeMap } from "rxjs/operators";
 const baseUrl = (url: string, path: string) => `${url.replace(/[¥/]$/, "")}/api/v1/${path}`;
-export const fetchData = <T>(url: string, path: string, config?: RequestInit) => fromFetch(baseUrl(url, path), config).pipe<T>(
-  mergeMap(res => res.json())
-)
+export const fetchData = <T>(url: string, path: string, config?: RequestInit) => fromFetch(baseUrl(url, path),
+  { ...config, headers: { "x-disable-pagination": "true", ...config?.headers } }).pipe<T>(
+    mergeMap(res => res.json())
+  )
